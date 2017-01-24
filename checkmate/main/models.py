166e2e1b2a96224288c2d74074de743537a3874a
Aman Gupta <aman060396@gmail.com>
2017-01-24 16:56:29 +0530

    Added Model for User Profile

--- checkmate/main/models.py
+++ checkmate/main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
 # Create your models here.
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(User) #extending user model
+	team_name = models.CharField(max_length=200)
+	name_1 = models.CharField(max_length=200)
+	idno_1 = models.CharField(max_length=20)
+	phone_1 = models.BigIntegerField(null=True)
+	email_1 = models.EmailField()
+	name_2 = models.CharField(max_length=200,blank=True)	
+	idno_2 = models.CharField(max_length=20,blank=True)
+	phone_2 = models.BigIntegerField(blank=True,null=True)
+	email_2 = models.EmailField(blank=True,null=True)
+	attempted_questions = models.CharField(max_length=59, default = "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0")
+	# List of questions attempted [0-Not Attempted, 1-Attempted once, 2-Attempted Twice, 3-Attempted Thrice/Blocked]
+	correct_questions = models.CharField(max_length=59, default = "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0")
+	# List of correctly attempted questions [0-Attempted Wrongly, 1-Attempted Correctly, 2-On Question]	
+	
+	def __str__(self):
+		return self.user.username
