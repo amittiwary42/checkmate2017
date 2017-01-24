84be722bfd5de77ac20420f7e2e17325c3881e7e
Aman Gupta <aman060396@gmail.com>
2017-01-24 17:06:25 +0530

    Created Team Form in forms.py

---
+++ checkmate/main/forms.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from django import forms
+from .models import *
+
+class TeamForm(forms.Form):
+	team_name = forms.CharField(max_length = 50,label = 'Team Name:')
+	password = forms.CharField(widget = forms.PasswordInput(), max_length = 50)
+	name_1 = forms.CharField(max_length = 50,label = 'Name of 1st Participant:')
+	idno_1 = forms.CharField(max_length = 12)
+	phone_1 = forms.IntegerField(widget = forms.TextInput(), min_value = 6000000000, label = 'Phone of 1st Participant:')
+	email_1 = forms.EmailField(label = 'Email of 1st Participant:')
+	name_2 = forms.CharField(max_length = 50, required = False, label = 'Name of 2nd Participant:')
+	idno_2 = forms.CharField(max_length = 12)
+	phone_1 = forms.IntegerField(widget = forms.TextInput(), min_value = 6000000000, label = 'Phone of 1st Participant:')
+	phone_2 = forms.IntegerField(widget = forms.TextInput(), required = False, min_value = 6000000000, label = 'Phone of 2nd Participant:')
+	email_2 = forms.EmailField(required = False, label = 'Email of 2nd Participant:')
+
+class LoginForm(forms.Form):
+	teamname = forms.CharField(max_length = 50)
+	password = forms.CharField(widget = forms.PasswordInput())
