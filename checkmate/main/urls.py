9d6c28da7977ec54e5d7ee6860fbf083e6c762aa
Aman Gupta <aman060396@gmail.com>
2017-01-26 16:11:42 +0530

    Mapped url for login page

--- checkmate/main/urls.py
+++ checkmate/main/urls.py
@@ -5,4 +5,6 @@ from main import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^index.html', views.index, name='index'),
+	url(r'^register.html', views.register, name='register')
+	url(r'^login.html', views.login, name='login'),
 ]
