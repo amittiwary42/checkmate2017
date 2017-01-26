7322a1df7548666a0bb3860ea54e8357d836b9ba
Aman Gupta <aman060396@gmail.com>
2017-01-26 16:16:34 +0530

    Removed redundancy for phone no. of participant 1

--- checkmate/main/forms.py
+++ checkmate/main/forms.py
@@ -11,7 +11,6 @@ class Team_Form(forms.Form):
 	email_1 = forms.EmailField(label = 'Email of 1st Participant:')
 	name_2 = forms.CharField(max_length = 50, required = False, label = 'Name of 2nd Participant:')
 	idno_2 = forms.CharField(max_length = 12)
-	phone_1 = forms.IntegerField(widget = forms.TextInput(), min_value = 6000000000, label = 'Phone No. of 1st Participant:')
 	phone_2 = forms.IntegerField(widget = forms.TextInput(), required = False, min_value = 6000000000, label = 'Phone of 2nd Participant:')
 	email_2 = forms.EmailField(required = False, label = 'Email of 2nd Participant:')
 
