b1b1f4f54a708a289c847fc7fa5af1bc97716f54
Aman Gupta <aman060396@gmail.com>
2017-02-28 18:03:48 +0530

    Fixed Rendering Bugs

--- checkmate/main/admin.py
+++ checkmate/main/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import *
 
 # Register your models here.
-admin.site.register(User_Profile)
-admin.site.register(Host)
-admin.site.register(Question)
-admin.site.register(City)
