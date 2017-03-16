fd99f4b9ccfe24ec28e35eb18f091f45b1eb66c3
Aman Gupta <aman060396@gmail.com>
2017-03-16 17:10:46 +0530

    Changed Databse to PostgreSQL

--- checkmate/checkmate/settings.py
+++ checkmate/checkmate/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'checkmate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'checkmate2017',
+        'USER': 'admin',
+        'PASSWORD': 'loremipsum',
+        'HOST': 'localhost',
+        'PORT':'',
     }
 }
 
